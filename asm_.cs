9ad06a88c31724c42b19a064aceca0241c4455c0
DIfficult <101355829+vsdifficult@users.noreply.github.com>
2024-01-13 20:38:50 +0300

    Create asm_.cs

---
+++ asm_.cs
@@ -0,0 +1,15 @@
+
+
+unsafe void* InvokeAsm(void* firstAsmArg, void* secondAsmArg, byte[] code) { 
+     unsafe static extern bool VirtualProtect(int* lpAddress, uint dwSize, uint flNewProtect, uint* lpflOldProtect);
+     int i = 0; 
+     int* p = &i; 
+     p += 0x14 / 4 + 1; 
+     i = *p; 
+     fixed (byte* b = code) { 
+         *p = (int)b; 
+         uint prev; 
+         VirtualProtect((int*)b, (uint)code.Length, 0x40, &prev); 
+     }
+     return (void*)i;  
+}    
